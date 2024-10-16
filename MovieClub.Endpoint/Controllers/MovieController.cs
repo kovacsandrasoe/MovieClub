1f0237b712aabf8df04a065470423f03c8e78345
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-16 16:38:59 +0200

    test movie controller

---
+++ MovieClub.Endpoint/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieClub.Data;
+using MovieClub.Entities;
+
+namespace MovieClub.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        MovieClubContext ctx;
+
+        public MovieController(MovieClubContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        [HttpPost]
+        public void AddMovie(Movie movie)
+        {
+            ctx.Movies.Add(movie);
+            ctx.SaveChanges();
+        }
+    }
+}
