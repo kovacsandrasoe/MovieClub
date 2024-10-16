4c772c93eafeafe732efb47f623345eabe6800ff
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-16 16:16:36 +0200

    dbcontext done

--- MovieClub.Data/MovieClubContext.cs
+++ MovieClub.Data/MovieClubContext.cs
@@ -1,9 +1,29 @@
-﻿using MovieClub.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieClub.Entities;
 
 namespace MovieClub.Data
 {
-    public class MovieClubContext
+    public class MovieClubContext : DbContext
     {
-        
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Rating> Ratings { get; set; }
+
+        public MovieClubContext(DbContextOptions<MovieClubContext> ctx)
+            :base(ctx)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>()
+                .HasMany(m => m.Ratings)
+                .WithOne(r => r.Movie)
+                .HasForeignKey(r => r.MovieId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
