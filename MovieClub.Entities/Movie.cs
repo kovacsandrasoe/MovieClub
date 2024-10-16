cb44283b3e0ffa7062decb07849183493ae25a3a
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-16 15:59:57 +0200

    movie done

--- MovieClub.Entities/Movie.cs
+++ MovieClub.Entities/Movie.cs
@@ -1,12 +1,30 @@
-﻿namespace MovieClub.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieClub.Entities
 {
     public class Movie
     {
+        public Movie(string title, string genre)
+        {
+            Id = Guid.NewGuid().ToString();
+            Title = title;
+            Genre = genre;
+        }
+
+        [StringLength(50)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
 
+        [StringLength(200)]
         public string Title { get; set; }
 
+        [StringLength(50)]
         public string Genre { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Rating>? Ratings { get; set; }
 
     }
 }
