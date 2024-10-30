170565f0ce175f990d280ad3525ad1d492484dce
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-30 17:02:21 +0100

    AutoMapper init

---
+++ MovieClub.Logic/DtoProvider.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MovieClub.Entities;
+using MovieClub.Entities.Dtos.Movie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieClub.Logic
+{
+    public class DtoProvider
+    {
+        public Mapper Mapper { get; }
+
+        public DtoProvider()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Movie, MovieShortViewDto>();
+                cfg.CreateMap<Movie, MovieViewDto>();
+                cfg.CreateMap<MovieCreateUpdateDto, Movie>();
+            });
+
+            Mapper = new Mapper(config);
+        }
+    }
+}
