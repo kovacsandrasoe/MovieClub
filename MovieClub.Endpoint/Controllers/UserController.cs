4bf29ed5e2609f4aa02682c7bcc3fe24230f0986
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-11-06 16:11:12 +0100

    usercreatedto

---
+++ MovieClub.Endpoint/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieClub.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public void Register()
+        {
+            //var user = new IdentityUser("geza");
+            
+            
+        }
+    }
+}
