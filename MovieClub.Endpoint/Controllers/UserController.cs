d5b0561435230141cfad0c51765d12a93d219c20
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-11-27 17:09:01 +0100

    grant and remove admin

--- MovieClub.Endpoint/Controllers/UserController.cs
+++ MovieClub.Endpoint/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using MovieClub.Entities.Dtos.User;
@@ -19,6 +20,26 @@ namespace MovieClub.Endpoint.Controllers
         {
             this.userManager = userManager;
             this.roleManager = roleManager;
+        }
+
+        [HttpGet("grantadmin/{userid}")]
+        [Authorize(Roles = "Admin")]
+        public async Task GrantAdmin(string userid)
+        {
+            var user = await userManager.FindByIdAsync(userid);
+            if (user == null)
+                throw new ArgumentException("User not found");
+            await userManager.AddToRoleAsync(user, "Admin");  
+        }
+
+        [HttpGet("revokeadmin/{userid}")]
+        [Authorize(Roles = "Admin")]
+        public async Task RevokeAdmin(string userid)
+        {
+            var user = await userManager.FindByIdAsync(userid);
+            if (user == null)
+                throw new ArgumentException("User not found");
+            await userManager.RemoveFromRoleAsync(user, "Admin");
         }
 
         [HttpPost("register")]
